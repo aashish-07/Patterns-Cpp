af2c5e32cbcf6833d48fd957da2938392f626cd3
Roman Empire <iaashish07@gmail.com>
2021-03-01 12:47:46 +0530

    intermediate

---
+++ pattern27.cpp
@@ -0,0 +1,131 @@
+/*
+
+*****
+*****
+*****
+*****
+******
+     *     *****
+     *     *****
+     ***********
+     *     *****
+     *     *****
+******
+*****
+*****
+*****
+*****
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// we observe that this pattern is horizontally symmetrical
+
+int main()
+{
+    // your code goes here
+    int n;
+    cin >> n;
+
+    // print square pattern with n-1 rows
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    // 	print n+1 stars
+    for (int i = 1; i <= n + 1; i++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+    // 	print star & spaces
+    for (int i = 1; i <= n / 2; i++)
+    { // 	print n spaces
+        for (int i = 1; i <= n; i++)
+        {
+            cout << " ";
+        }
+
+        // print star
+        cout << "*";
+
+        // print n spaces
+        for (int i = 1; i <= n; i++)
+        {
+            cout << " ";
+        }
+
+        // 	print n stars
+        for (int i = 1; i <= n; i++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // 	print n spaces
+    for (int i = 1; i <= n; i++)
+    {
+        cout << " ";
+    }
+
+    // 	 print n*2+1 stars
+    for (int i = 1; i <= n * 2 + 1; i++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+    // 	print star & spaces
+    for (int i = 1; i <= n / 2; i++)
+    { // 	print n spaces
+        for (int i = 1; i <= n; i++)
+        {
+            cout << " ";
+        }
+
+        // print star
+        cout << "*";
+
+        // print n spaces
+        for (int i = 1; i <= n; i++)
+        {
+            cout << " ";
+        }
+
+        // 	print n stars
+        for (int i = 1; i <= n; i++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // 	print n+1 stars
+    for (int i = 1; i <= n + 1; i++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+    // 	print square pattern with n-1 rows
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
